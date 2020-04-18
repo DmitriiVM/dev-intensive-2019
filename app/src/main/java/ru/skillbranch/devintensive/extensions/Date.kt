29d418d31c68193c382a04f09a924fe791a96a76
Dmitry <mdimail@list.ru>
2020-04-18 23:32:31 +0400

    hometask 2 complete

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -32,31 +32,47 @@ fun Date.humanizeDiff(date: Date = Date()): String {
         in 0..SECOND -> "только что"
         in SECOND..45 * SECOND -> "несколько секунд назад"
         in 45 * SECOND..75 * SECOND -> "минуту назад"
-        in 75 * SECOND..45 * MINUTE -> "${TimeUnits.MINUTE.plural((diff / MINUTE).toInt())} назад"
+        in 75 * SECOND..45 * MINUTE -> " назад"
         in 45 * MINUTE..75 * MINUTE -> "час назад"
-        in 75 * MINUTE..22 * HOUR -> "${TimeUnits.HOUR.plural((diff / HOUR).toInt())} назад"
+        in 75 * MINUTE..22 * HOUR -> " назад"
         in 22 * HOUR..26 * HOUR -> "день назад"
-        in 26 * HOUR..360 * DAY -> "${TimeUnits.DAY.plural((diff / DAY).toInt())} назад"
+        in 26 * HOUR..360 * DAY -> " назад"
         else -> "более года назад"
     }
 }
 
-fun TimeUnits.plural(value: Int): String {
-    val lastTwoNumbers = value.toString().takeLast(2).toInt()
-    val lastNumber = value.toString().last().toString().toInt()
-    val strings = when (this) {
-        TimeUnits.SECOND -> arrayListOf("секунд", "секунду", "секунды")
-        TimeUnits.MINUTE -> arrayListOf("минут", "минуту", "минуты")
-        TimeUnits.HOUR -> arrayListOf("часов", "час", "часа")
-        TimeUnits.DAY -> arrayListOf("дней", "день", "дня")
-    }
-    return when {
-        lastTwoNumbers in 11..14 -> "$value ${strings[0]}"
-        lastNumber == 1 -> "$value ${strings[1]}"
-        lastNumber in 2..4 -> "$value ${strings[2]}"
-        else -> "$value ${strings[0]}"
-    }
-}
+//fun Date.humanizeDiff(date: Date = Date()): String {
+//    val diff = date.time - time
+//    if (diff < 0) return "Некорректная дата"
+//    return when (diff) {
+//        in 0..SECOND -> "только что"
+//        in SECOND..45 * SECOND -> "несколько секунд назад"
+//        in 45 * SECOND..75 * SECOND -> "минуту назад"
+//        in 75 * SECOND..45 * MINUTE -> "${TimeUnits.MINUTE.plural((diff / MINUTE).toInt())} назад"
+//        in 45 * MINUTE..75 * MINUTE -> "час назад"
+//        in 75 * MINUTE..22 * HOUR -> "${TimeUnits.HOUR.plural((diff / HOUR).toInt())} назад"
+//        in 22 * HOUR..26 * HOUR -> "день назад"
+//        in 26 * HOUR..360 * DAY -> "${TimeUnits.DAY.plural((diff / DAY).toInt())} назад"
+//        else -> "более года назад"
+//    }
+//}
+
+//fun TimeUnits.plural(value: Int): String {
+//    val lastTwoNumbers = value.toString().takeLast(2).toInt()
+//    val lastNumber = value.toString().last().toString().toInt()
+//    val strings = when (this) {
+//        TimeUnits.SECOND -> arrayListOf("секунд", "секунду", "секунды")
+//        TimeUnits.MINUTE -> arrayListOf("минут", "минуту", "минуты")
+//        TimeUnits.HOUR -> arrayListOf("часов", "час", "часа")
+//        TimeUnits.DAY -> arrayListOf("дней", "день", "дня")
+//    }
+//    return when {
+//        lastTwoNumbers in 11..14 -> "$value ${strings[0]}"
+//        lastNumber == 1 -> "$value ${strings[1]}"
+//        lastNumber in 2..4 -> "$value ${strings[2]}"
+//        else -> "$value ${strings[0]}"
+//    }
+//}
 
 enum class TimeUnits {
     SECOND,
